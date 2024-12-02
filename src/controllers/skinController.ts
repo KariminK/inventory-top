3ea09ce6b8cf2de3bbeb9886b52214f7cb151f5f
KariminK <103779342+KariminK@users.noreply.github.com>
2024-12-02 19:55:20 +0100

    validation for new skin

--- src/controllers/skinController.ts
+++ src/controllers/skinController.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
-import { skin } from "../types";
+import { quality, skin } from "../types";
+import SkinModel from "../models/SkinModel";
+import { body, validationResult } from "express-validator";
 
 export const getIndex: RequestHandler = (req, res) => {
   res.render("index");
@@ -7,7 +9,47 @@ export const getIndex: RequestHandler = (req, res) => {
 
 export const getNewSkin: RequestHandler = (req, res) => {};
 
-export const postNewSkin: RequestHandler = (req, res) => {};
+// post new skin
+const postNewSkinReqHandler: RequestHandler = (req, res) => {
+  const data = validationResult(req);
+  console.log(data);
+};
+
+// validator
+function isQuality(qual: string): qual is quality {
+  return [
+    "Battle-Scared",
+    "Well-Worn",
+    "Field-Tested",
+    "Minimal Wear",
+    "Factory New",
+  ].includes(qual);
+}
+
+// TO DO: TEST THIS
+export const postNewSkin = [
+  body("weapon")
+    .trim()
+    .notEmpty()
+    .withMessage("weapon name cannot be empty")
+    .isString()
+    .withMessage("weapon name must be text"),
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("skin name cannot be empty")
+    .isString()
+    .withMessage("skin name must be text"),
+  body("quality")
+    .trim()
+    .notEmpty()
+    .withMessage("skin quality cannot be empty")
+    .custom(isQuality)
+    .withMessage(
+      "Skin Qualities are: Battle-Scared, Well-Worn, Field-Tested, Minimal Wear, Factory New"
+    ),
+  postNewSkinReqHandler,
+];
 
 export const getEditSkin: RequestHandler = (req, res) => {};
 export const putEditSkin: RequestHandler = (req, res) => {};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,13 @@
+export type quality =
+  | "Battle-Scared"
+  | "Well-Worn"
+  | "Field-Tested"
+  | "Minimal Wear"
+  | "Factory New";
 export interface skin {
   id?: number;
   weapon: string;
   name: string;
-  quality:
-    | "Battle-Scared"
-    | "Well-Worn"
-    | "Field-Tested"
-    | "Minimal Wear"
-    | "Factory New";
+  quality: quality;
   collection: string;
 }
